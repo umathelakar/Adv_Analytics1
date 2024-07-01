1ee7e04b5363056265a335b752f94a55416e1f8a
umathelakar <146862534+umathelakar@users.noreply.github.com>
2024-07-02 01:09:20 +0530

    Update BarChart.jsx

--- BarChart.jsx
+++ BarChart.jsx
@@ -35,3 +35,64 @@ const BarChart = ({ data }) => {
 export default BarChart;
 
 
+///////////////////////////////////////////////////////////////////////////////
+// BarChart.jsx
+
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+
+const BarChart = ({ data }) => {
+  const chartData = {
+    labels: Object.keys(data),
+    datasets: [
+      {
+        label: 'Stocks',
+        data: Object.values(data).map((stateData) => stateData.Stocks),
+        backgroundColor: '#FF6384',
+      },
+      {
+        label: 'Bonds',
+        data: Object.values(data).map((stateData) => stateData.Bonds),
+        backgroundColor: '#36A2EB',
+      },
+      {
+        label: 'ETFs',
+        data: Object.values(data).map((stateData) => stateData.ETFs),
+        backgroundColor: '#FFCE56',
+      },
+    ],
+  };
+
+  const options = {
+    plugins: {
+      title: {
+        display: true,
+        text: 'US States vs Products Handled',
+      },
+      legend: {
+        display: true,
+        position: 'top',
+      },
+    },
+    scales: {
+      x: {
+        stacked: true,
+        title: {
+          display: true,
+          text: 'States',
+        },
+      },
+      y: {
+        stacked: true,
+        title: {
+          display: true,
+          text: 'Products Handled',
+        },
+      },
+    },
+  };
+
+  return <Bar data={chartData} options={options} />;
+};
+
+export default BarChart;
