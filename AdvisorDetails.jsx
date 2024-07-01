77dce4e072f2187ffd4d1c73327603d3cede0638
umathelakar <146862534+umathelakar@users.noreply.github.com>
2024-07-02 00:14:53 +0530

    Update AdvisorDetails.jsx

--- AdvisorDetails.jsx
+++ AdvisorDetails.jsx
@@ -2,21 +2,30 @@
 
 import React from 'react';
 
-const AdvisorDetails = ({ advisor }) => {
+const AdvisorDetails = ({ advisor, advisorName }) => {
   return (
     <div className="advisor-details">
       <h2>Advisor Details</h2>
       {advisor ? (
         <div>
-          <p>Advisor Name: {advisor.name}</p>
-          <p>Rep Code: {advisor.repCode}</p>
-          <p>Employee ID: {advisor.empId}</p>
+          <p>Advisor Name: {advisorName}</p>
+          <p>Rep Code: {advisor.rep_code}</p>
+          <p>Employee ID: {advisor.emp_id}</p>
           <p>Customers:</p>
-          <ul>
-            {advisor.customers.map((customer, index) => (
-              <li key={index}>{customer}</li>
-            ))}
-          </ul>
+          <table className="customer-table">
+            <thead>
+              <tr>
+                <th>Customer Name</th>
+              </tr>
+            </thead>
+            <tbody>
+              {advisor.customers.map((customer, index) => (
+                <tr key={index}>
+                  <td>{customer}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       ) : (
         <p>No advisor data found</p>
@@ -26,4 +35,6 @@ const AdvisorDetails = ({ advisor }) => {
 };
 
 export default AdvisorDetails;
+
+
 
